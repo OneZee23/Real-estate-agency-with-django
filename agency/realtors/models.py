fa779922d1abc510ed9a0204a9cc7e3f6e5449e4
Nikita Shevelev <nikita.shevelev@ripecode.dev>
2021-05-10 12:35:08 +0300

    Edit readme

--- agency/realtors/models.py
+++ agency/realtors/models.py
@@ -4,11 +4,11 @@ from datetime import datetime
 
 class Realtor(models.Model):
   name = models.CharField("Имя", max_length=200)
-  photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+  photo = models.ImageField("Фото", upload_to='photos/%Y/%m/%d/')
   description = models.TextField("Описание", null=True, blank=True)
   phone = models.CharField("Телефон", max_length=200)
   email = models.CharField("Электронная почта", max_length=200)
-  is_mvp = models.BooleanField(default=False)
+  is_mvp = models.BooleanField("Работает в данный момент?", default=False)
   hire_date = models.DateTimeField("Дата найма", default=datetime.now, null=True, blank=True)
 
   def __str__(self):
